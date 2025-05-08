3c031875f928b66fe6e520f5f483286cd7e96ba4
youngpotiune <526SMontecalvo@frhsd.com>
2025-05-08 09:44:19 -0400

    444444

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Random Quotes for Hells Angels
+
 const quotes = [
   '"When you go to the top, you realize that the road up there is paved with dead bodies." - Sonny Barger',
   '"I don\'t believe in God, but I believe in the Hells Angels." - Unknown',
   '"The Angels are the last of the true outlaws." - Rolling Stone Magazine',
   '"Freedom is not free. It is earned." - Unknown',
   '"We are not a gang. We are a brotherhood." - Anonymous'
+  
+  
 ];
 
-// Function to change quote every 5 seconds
+
 let currentQuoteIndex = 0;
 const quoteElement = document.getElementById('quote');
 
@@ -37,7 +39,7 @@ prevButton.addEventListener('click', () => {
     updateCarousel(currentIndex);
 });
 
-// Optionally add auto-slide functionality for smoother experience
+
 setInterval(() => {
     currentIndex = (currentIndex + 1) % slides.length;
     updateCarousel(currentIndex);
